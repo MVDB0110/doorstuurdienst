d8b3aa2bf2a20999f57b1da0944d41a2356ef429
Mike van den Brink <31769246+MVDB0110@users.noreply.github.com>
2019-11-20 15:01:53 +0100

    Update forwards.py
    Rollback to python timestamp

--- forwards.py
+++ forwards.py
@@ -26,6 +26,7 @@ def readone(id):
 def post(forward):
     bron = forward.get("bron", None)
     doel = forward.get("doel", None)
+    forward.update({'timestamp': time.time()})
 
     archiveforwards = Forward.query.filter(Forward.bron == bron).filter(Forward.provision == 'absent').filter(Forward.archive == 'n').all()
     for archiveforward in archiveforwards:
