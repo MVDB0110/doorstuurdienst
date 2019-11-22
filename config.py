cf362a864d66908cfe3a420cd9a264a0cca80557
Mike van den Brink <31769246+MVDB0110@users.noreply.github.com>
2019-11-22 12:10:33 +0100

    Update config.py

--- config.py
+++ config.py
@@ -17,7 +17,7 @@ connex_app = connexion.App(__name__, specification_dir=basedir)
 app = connex_app.app
 
 app.config['SQLALCHEMY_ECHO'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://api:password@10.0.0.254:3306/forward'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://api:password@127.0.0.1:3306/forward'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
